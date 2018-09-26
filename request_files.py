71fa2c767313f6424297bab1e649e0b85806962f
André de Sousa Araújo <dedeco@gmail.com>
2018-09-26 11:59:46 -0300

    Updating scripts

--- request_files.py
+++ request_files.py
@@ -12,22 +12,24 @@ def retrieve_interim():
        In the example below the data are organised in files per month. (eg "interim_daily_201510.nc")
     """
     yearStart = 2007
-    yearEnd = 2007
-    monthStart = 3
-    monthEnd = 3
+    yearEnd = 2016
+    months = [1,2,3,10,11,12]
     for year in list(range(yearStart, yearEnd + 1)):
-        for month in list(range(monthStart, monthEnd + 1)):
+        for month in months:
             startDate = '%04d%02d%02d' % (year, month, 1)
             numberOfDays = calendar.monthrange(year, month)[1]
             lastDate = '%04d%02d%02d' % (year, month, numberOfDays)
-            target = "_interim_daily_%04d%02d.nc" % (year, month)
+            target = "_interim_daily_grid_075_sfc_%04d%02d.nc" % (year, month)
             requestDates = (startDate + "/TO/" + lastDate)
             interim_request(requestDates, target)
  
-def interim_request_surface(requestDates, target):
+def interim_request_sfc(requestDates, target):
     """      
         An ERA interim request for analysis surface level data.
     """
+
+server.retrieve({   
+
     server.retrieve({
         "class": "ei",
         "stream": "oper",
@@ -35,14 +37,14 @@ def interim_request_surface(requestDates, target):
         "dataset": "interim",
         "date": requestDates,
         "expver": "1",
-        "step": "3/6/9/12",
-        "levtype": "fc",
+        "step": "0",
+        "levtype": "sfc",
         "area": "0/-60/-30/0",
-#        "param": "31.128/32.128/33.128/34.128/35.128/36.128/37.128/38.128/39.128/40.128/41.128/42.128/53.162/54.162/55.162/56.162/57.162/58.162/59.162/60.162/61.162/62.162/63.162/64.162/65.162/66.162/67.162/68.162/69.162/70.162/71.162/72.162/73.162/74.162/75.162/76.162/77.162/78.162/79.162/80.162/81.162/82.162/83.162/84.162/85.162/86.162/87.162/88.162/89.162/90.162/91.162/92.162/134.128/136.128/137.128/139.128/141.128/148.128/151.128/164.128/165.128/166.128/167.128/168.128/170.128/173.128/174.128/183.128/186.128/187.128/188.128/198.128/206.128/234.128/235.128/236.128/238.128",
+        "param": "31.128/32.128/33.128/34.128/35.128/36.128/37.128/38.128/39.128/40.128/41.128/42.128/53.162/54.162/55.162/56.162/57.162/58.162/59.162/60.162/61.162/62.162/63.162/64.162/65.162/66.162/67.162/68.162/69.162/70.162/71.162/72.162/73.162/74.162/75.162/76.162/77.162/78.162/79.162/80.162/81.162/82.162/83.162/84.162/85.162/86.162/87.162/88.162/89.162/90.162/91.162/92.162/134.128/136.128/137.128/139.128/141.128/148.128/151.128/164.128/165.128/166.128/167.128/168.128/170.128/173.128/174.128/183.128/186.128/187.128/188.128/198.128/206.128/234.128/235.128/236.128/238.128",
         "format": "netcdf",
         "target": target,
         "time": "00/06/12/18",
         "grid": "0.75/0.75"
     })
 if __name__ == '__main__':
-    interim_request_surface()
+    interim_request_sfc()
